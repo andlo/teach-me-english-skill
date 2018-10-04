5f94629a92f1ea45a651bed3878a4b9c6ff4e0a8
Andreas Lorensen <andlo@outlook.dk>
2018-10-04 21:50:59 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -8,7 +8,18 @@ class TeachMeEnglish(MycroftSkill):
     @intent_file_handler('english.me.teach.intent')
     def handle_english_me_teach(self, message):
         self.speak_dialog('english.me.teach')
+        lesson = self.get_response("repeat")
 
+ 
+ 
+    @intent_file_handler('teaching.intent')
+    def  teaching_intent(self, message):
+        # response = {'name': message.data.get("name")}
+        if message == lesson:
+            self.speak_dialog("perfect")
+        else:
+            self.speak_dialog("notperfect")
+            self.speak(lesson)
 
 def create_skill():
     return TeachMeEnglish()
