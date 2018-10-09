525b9391021c1410f7e5c9a8c12546f92951baeb
Andreas Lorensen <andlo@outlook.dk>
2018-10-09 17:24:10 +0200

    initial release

--- __init__.py
+++ __init__.py
@@ -2,24 +2,48 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class TeachMeEnglish(MycroftSkill):
+    def initialize(self):
+        self.add_event("speak", self.capture_speaks)
+        self.last_speech = ""
+        self.capture = False
+
+    def capture_speaks(self, message):
+        if self.capture == True:
+            self.last_speech = message.data.get("utterance", "")
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('english.me.teach.intent')
-    def handle_english_me_teach(self, message):
-        self.speak_dialog('english.me.teach')
-        lesson = self.get_response("repeat")
-
- 
- 
     @intent_file_handler('teaching.intent')
-    def  teaching_intent(self, message):
-        # response = {'name': message.data.get("name")}
-        if message == lesson:
-            self.speak_dialog("perfect")
-        else:
-            self.speak_dialog("notperfect")
-            self.speak(lesson)
+    def handle_teaching(self, message):
+        user_said = None
+        self.capture = True
+        while (user_said != "stop"):   
+            self.speak_dialog('repeat_after_me')
+            user_said = self.get_response("lesson",num_retries=0)    
+            lesson = self.last_speech
+            retries = 3
+            while lesson != None and user_said != "stop" and retries != 0:          
+                if user_said and user_said == self.last_speech:
+                    self.speak_dialog("perfect")
+                    lesson = None
+                elif user_said == None:
+                    retries = retries - 1
+                    if retries == 0:
+                        user_said = "stop"
+                    else:
+                        self.speak_dialog("repeat_after_me")
+                        user_said = self.get_response(lesson,num_retries=0)    
+                else:
+                    retries = retries - 1
+                    self.speak_dialog("notperfect")
+                    self.speak_dialog("repeat_after_me")
+                    user_said = self.get_response(lesson,num_retries=0)    
+            
+            self.speak_dialog("lets_try_another_one")
+
+        self.speak_dialog('teaching_stop')
+        self.capture = False
 
 def create_skill():
     return TeachMeEnglish()
