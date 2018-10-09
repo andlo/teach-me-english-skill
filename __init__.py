116332c5a8448706723b9256b2e9b2d66e3f9cfa
Andreas Lorensen <andlo@outlook.dk>
2018-10-09 17:31:04 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -19,30 +19,30 @@ class TeachMeEnglish(MycroftSkill):
         user_said = None
         self.capture = True
         while (user_said != "stop"):   
-            self.speak_dialog('repeat_after_me')
+            self.speak_dialog('repeat_after_me', expect_response=False)
             user_said = self.get_response("lesson",num_retries=0)    
             lesson = self.last_speech
             retries = 3
             while lesson != None and user_said != "stop" and retries != 0:          
                 if user_said and user_said == self.last_speech:
-                    self.speak_dialog("perfect")
+                    self.speak_dialog("perfect", expect_response=False)
                     lesson = None
                 elif user_said == None:
                     retries = retries - 1
                     if retries == 0:
                         user_said = "stop"
                     else:
-                        self.speak_dialog("repeat_after_me")
+                        self.speak_dialog("repeat_after_me", expect_response=False)
                         user_said = self.get_response(lesson,num_retries=0)    
                 else:
                     retries = retries - 1
-                    self.speak_dialog("notperfect")
-                    self.speak_dialog("repeat_after_me")
+                    self.speak_dialog("notperfect", expect_response=False)
+                    self.speak_dialog("repeat_after_me", expect_response=False)
                     user_said = self.get_response(lesson,num_retries=0)    
             
-            self.speak_dialog("lets_try_another_one")
+            self.speak_dialog("lets_try_another_one", expect_response=False)
 
-        self.speak_dialog('teaching_stop')
+        self.speak_dialog('teaching_stop', expect_response=False)
         self.capture = False
 
 def create_skill():
